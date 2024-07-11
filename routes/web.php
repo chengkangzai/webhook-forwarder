75868fea265b0341f69d0bf1e93799bdb80894e4
chengkangzai <pycck@hotmail.com>
2024-07-11 20:22:12 +0800

    do not verify green api webhook

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
 use App\Http\Controllers\WebhookController;
-use App\Http\Middlewares\VerifyGreenApiWebhookMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/webhook', WebhookController::class)
-    ->middleware(VerifyGreenApiWebhookMiddleware::class);
+Route::post('/webhook', WebhookController::class);
 
 Route::fallback(function () {
     return redirect()->to(filament()->getUrl());
