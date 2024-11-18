75079074f07cb43bb0f1233fdfd095757924c064
chengkangzai <pycck@hotmail.com>
2024-11-18 16:57:32 +0800

    feat: Integrate GreenApi and update header actions in ViewInstance page    - Added GreenApi, Instance, and Filament\Actions\Action to the ViewInstance class.    - Implemented a new action 'link_to_forwarder' that updates the Green API settings for an instance when triggered.    - Updated the webhook route with a name attribute ('webhook') for easier referencing.

--- app/Filament/Resources/InstanceResource/Pages/ViewInstance.php
+++ app/Filament/Resources/InstanceResource/Pages/ViewInstance.php
@@ -3,7 +3,11 @@
 namespace App\Filament\Resources\InstanceResource\Pages;
 
 use App\Filament\Resources\InstanceResource;
+use App\Integrations\GreenApi\GreenApi;
+use App\Models\Instance;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewInstance extends ViewRecord
@@ -14,6 +18,23 @@ class ViewInstance extends ViewRecord
     {
         return [
             DeleteAction::make(),
+            Action::make('link_to_forwarder')
+                ->action(function (Instance $instance) {
+                    $greenApi = new GreenApi($instance->instance_id, $instance->instance_token);
+
+                    $greenApi->account()->setAccountSettings(
+                        webhookUrl: route('webhook'),
+                        webhookUrlToken: config('services.green-api.secret'),
+                        outgoingWebhook: 'yes',
+                        outgoingApimessageWebhook: 'yes',
+                    );
+
+                    Notification::make('success')
+                        ->success()
+                        ->title('Success!')
+                        ->body('Green API setting has been updated.')
+                        ->send();
+                })
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/webhook', WebhookController::class);
+Route::post('/webhook', WebhookController::class)->name('webhook');
 
 Route::fallback(function () {
     return redirect()->to(filament()->getUrl());
