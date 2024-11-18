663fd6dca41734f94dc373591f5157e595e789ee
chengkangzai <pycck@hotmail.com>
2024-11-18 17:11:28 +0800

    introduce horizon

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('model:prune')->weekly();
+
+Schedule::command('horizon:snapshot')->everyFiveMinutes();
